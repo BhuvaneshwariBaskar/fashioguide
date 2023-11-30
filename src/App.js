e481fc147e3ca9274679e04b7eb09b417c041fec
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-11-30 19:38:04 +0530

    Changes in navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './Pages/Home';
+import Home from './pages/Home';
   
 function App() {
   return (
