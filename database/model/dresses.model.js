39b5965f5e552cdb703e43a2ce11f983da3bef6a
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-12-13 18:54:20 +0530

    database connection over

--- database/model/dresses.model.js
+++ database/model/dresses.model.js
@@ -17,7 +17,7 @@ const dressModel = (sequelize) => {
     },
 
     category_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
       allowNull: false,
       references: {
           model: 'Categories', // This is the name of the target model (case-sensitive)
