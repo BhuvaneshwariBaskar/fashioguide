6441d53d635b31aee576f90e884039ed133fae00
gopika~ <gopikaks.cse2020@citchennai.net>
2024-01-17 11:41:44 +0530

    categoryurl

--- src/utils/category.js
+++ src/utils/category.js
@@ -1,66 +1,75 @@
 export const Category = [
     {
         category_id:3601,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_01_fad87cb7-ebc1-4531-b3ad-18b7021e7287.jpg?v=1700213445",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20220309/inTn/6227b81faeb26921afcde577/-286Wx359H-441137362-white-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Female",
-        dresses:["4601","4602"]
+        
     },
     {
         category_id:3602,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_02_8e32be3d-535c-4729-b3c4-9a34952e1a8c.jpg?v=1700213549",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20220120/lLur/61e981aef997dd66232f4792/-286Wx359H-441036730-charcoal-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Female",
-        dresses:["4601","4602"]
+       
     },
     {
         category_id:3603,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_03_1ce3c1f6-b934-42e0-82f0-f8fd6e78fc76.jpg?v=1700213495",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20220125/5ENA/61eef659aeb2695cdd2acad3/-286Wx359H-441128501-jetblack-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Female",
-        dresses:["4601","4602"]
+        
     },
     {
         category_id:3604,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_01_fad87cb7-ebc1-4531-b3ad-18b7021e7287.jpg?v=1700213445",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/h97/h0d/13480454914078/-286Wx359H-441000453-red-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Female",
-        dresses:["4601","4602"]
+        
     },
     {
         category_id:3605,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_02_8e32be3d-535c-4729-b3c4-9a34952e1a8c.jpg?v=1700213549",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20220301/DDoY/621e35edaeb26921afbf005b/-286Wx359H-441137352-olive-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Female",
-        dresses:["4601","4602"]
+     
     },
     {
         category_id:3606,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_03_1ce3c1f6-b934-42e0-82f0-f8fd6e78fc76.jpg?v=1700213495",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/hf3/h40/15590223183902/-286Wx359H-441000674-maroon-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Female",
-        dresses:["4601","4602"]
+    
     },
     {
         category_id:3607,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_01_fad87cb7-ebc1-4531-b3ad-18b7021e7287.jpg?v=1700213445",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20210828/0ozJ/61297a6f7cdb8cb8240b1361/-286Wx359H-460856330-black-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Male",
-        dresses:["4601","4602"]
+        
     },
     {
         category_id:3608,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_02_8e32be3d-535c-4729-b3c4-9a34952e1a8c.jpg?v=1700213549",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20210225/Q90z/6037be57f997dd5c40fbf0bf/-286Wx359H-441120075-navy-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Male",
-        dresses:["4601","4602"]
+       
     },
     {
         category_id:3609,
-        image:"https://www.westside.com/cdn/shop/files/L1_Quick_Link_03_1ce3c1f6-b934-42e0-82f0-f8fd6e78fc76.jpg?v=1700213495",
+        image:
+      "https://assets.ajio.com/medias/sys_master/root/20211019/U4It/616dda2eaeb26901109a9256/-286Wx359H-441113673-navy-MODEL.jpg",
         category_name:"Kurta",
         gender_type:"Male",
-        dresses:["4601","4602"]
+        
     }
 ]
 export const baseurl = "http://localhost:8080"
