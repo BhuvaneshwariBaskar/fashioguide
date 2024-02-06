bde36535dffc3d242ef4234166b72b8c70ba87de
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-02-06 16:17:56 +0530

    tryOn

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
-import Collection from "./pages/Collection";
-import Individual from "./pages/Individual";
+import Collection from "./Pages/Collection";
+import Individual from "./Pages/Individual";
 import SingleCollection from "./Pages/SingleCollection";
 import Category from "./Pages/Category";
 import Cart from "./Pages/Cart";
@@ -12,7 +12,7 @@ import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import Contact from "./Pages/Contact";
 import { useSelector } from "react-redux";
-import TryOn from "./pages/TryOn";
+import TryOn from "./Pages/TryOn";
 
 function App() {
   const { user } = useSelector((state) => ({ ...state }));
@@ -24,9 +24,12 @@ function App() {
           <>
             <Route path="/" element={<Home />} />
             <Route path="/individual" element={<Individual />} />
-            <Route path="/singlecollection" element={<SingleCollection user={user}/>} />
+            <Route
+              path="/singlecollection"
+              element={<SingleCollection user={user} />}
+            />
             <Route path="/category" element={<Category />} />
-            <Route path="/collections" element={<Collection user={user}/>} />
+            <Route path="/collections" element={<Collection user={user} />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/contact" element={<Contact />} />
@@ -38,9 +41,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Login />} />
             <Route path="*" element={<div>404 - Page Not Found</div>} />
-            
-           
-        
           </>
         )}
       </Routes>
